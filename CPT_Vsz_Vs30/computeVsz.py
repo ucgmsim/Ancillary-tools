a58a562b4d2fbc68d2a13630a28283e8488d76ca
Jason <25143301+jasonmotha@users.noreply.github.com>
2020-11-04 09:31:03 +1300

    additionalfiltering

--- CPT_Vsz_Vs30/computeVsz.py
+++ CPT_Vsz_Vs30/computeVsz.py
@@ -22,6 +22,11 @@ def compute_vsz_sigma_from_vs(Vs, randVs, z, correlationFlag):
         d = 0
         t = 0
         n = 0
+
+        cur_depth = 0
+        prev_depth = None
+        cur_vs = prev_vs = Vs[0]
+
         while z[n] < max_depth:
             dn = z[n + 1] - z[n]
             vn = 0.5 * (randVs[n, k] + randVs[n + 1, k])  # velocity at mid point
@@ -49,9 +54,18 @@ def compute_vsz_from_vs(Vs, z):
     d = 0
     t = 0
     n = 0
-    while z[n] < max_depth:
-        dn = z[n + 1] - z[n]
-        vn = 0.5 * (Vs[n] + Vs[n + 1])  # velocity at mid point
+    cur_depth = 0
+    prev_depth = None
+    cur_vs = prev_vs = Vs[0]
+    while cur_depth < max_depth:
+        prev_depth = cur_depth
+        cur_depth = z[n]
+        dn = cur_depth - prev_depth
+
+        prev_vs = cur_vs
+        cur_vs = Vs[n]
+
+        vn = 0.5 * (cur_vs + prev_vs)  # velocity at mid point
         tn = dn / vn
         t += tn
         d += dn
--- CPT_Vsz_Vs30/nz_cpt_process.py
+++ CPT_Vsz_Vs30/nz_cpt_process.py
@@ -1,46 +1,85 @@
+from collections import Counter
 import os
 from pathlib import Path
 
+from mpi4py import MPI
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
+from qcore import geo
 from getCPTdata import getCPTdata
 from computeVs import Vs_McGann
 from computeVsz import compute_vsz_from_vs
 from computeVs30 import vsz_to_vs30
 
-out_dir = Path("/home/jam335/scratch/vs30/cpt2vs30/")
+
+def log_error(skipped_fp, cpt_name, error):
+    skipped_fp.write(f"{cpt_name} - {error}\n")
+
+
+def count_digits(arr):
+    stringified = str(arr).replace("0", "").replace(".", "")
+    return Counter(stringified)
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+hostname = MPI.Get_processor_name()
+master = 0
+is_master = not rank
+
+out_dir = Path("/home/jam335/scratch/syncthing/vs30/cpt2vs30")
 plot_dir = out_dir / "validation_plots"
 
 cpt_df = pd.read_csv("/isilon/vs30/input_data/TTGD_CPT_sCPT_Download_2020-09-11/Metadata/cpt_locations_20200909.csv", sep=",")
 
-cpt_root_path = Path("/isilon/vs30/input_data/TTGD_CPT_sCPT_Download_2020-09-11/CPT_Depth_Profile_CSVs/")
+cpt_root_path = Path("/home/jam335/scratch/syncthing/vs30/cpt2vs30/CPT_Depth_Profile_CSVs")
 
 results = {}
 
-skipped_fp = open(out_dir / "skipped_cpts", "w")
+skipped_fp = open(out_dir / "skipped_cpts" / f"skipped_cpts_{rank}", "w")
 
 for row_n, cpt in cpt_df.iterrows():
+    if row_n % size != rank:
+        continue
     dir = f"{'Private' if cpt['TTGD Only'] else 'Public'}_{'sCPT' if cpt.InvestigationType == 'SCPT' else cpt.InvestigationType}"
-    fname = f"{cpt.CombinedName}.csv"
-    print(row_n, cpt.CombinedName)
+    cpt_name = cpt.CombinedName
+    fname = f"{cpt_name}.csv"
+    print(f"rank: {rank} -- ", row_n, cpt_name)
     cpt_ffp = cpt_root_path / dir / fname
 
     if os.path.exists(cpt_ffp):
         try:
             (z, qc, fs, u2) = getCPTdata(cpt_ffp)
         except (ValueError, Exception) as e:
-            skipped_fp.write(f"{cpt.CombinedName} - could not read file: {str(e)}\n")
+            log_error(skipped_fp, cpt_name, f"could not read file: {str(e)}")
+            continue
+
+        # duplicate depth check
+        u, c = np.unique(z, return_counts=True)
+        if np.any([c > 1]):
+            log_error(skipped_fp, cpt_name, f"Duplicate depth detected - invalid CPT")
+            continue
+
+        # Check for invalid negative readings
+        if any(fs < -0.2) or any(qc < -0.2) or any(u2 < -0.2):
+            log_error(skipped_fp, cpt_name, f"negative value - discarding")
+            continue
+
+        # Check for repeated digits
+        if any(value > 3 for fs_value in fs for key, value in count_digits(fs_value)):
+            log_error(skipped_fp, cpt_name, f"Repeated digit - investigating")
             continue
 
         max_depth = max(z)
         if max_depth < 5:
-            skipped_fp.write(f"{cpt.CombinedName} - depth<5: {max_depth}\n")
+            log_error(skipped_fp, cpt_name, f"depth<5: {max_depth}")
             continue
-        z_span = max_depth - min(z)
+        min_depth = min(z)
+        z_span = max_depth - min_depth
         if z_span < 5:
-            skipped_fp.write(f"{cpt.CombinedName} - depth range <5: {z_span}\n")
+            log_error(skipped_fp, cpt_name, f"depth range <5: {z_span}")
             continue
         (z, Vs, Vs_SD) = Vs_McGann(z, qc, fs)
         Vsz, max_depth = compute_vsz_from_vs(Vs, z)
@@ -52,8 +91,9 @@ for row_n, cpt in cpt_df.iterrows():
         vs30_result["Vsz"] = Vsz
         vs30_result["Vs30"] = vs30
         vs30_result["Zmax"] = max_depth
+        vs30_result["Zmin"] = min_depth
         vs30_result["Zspan"] = z_span
-        results[cpt.CombinedName] = vs30_result
+        results[cpt_name] = vs30_result
 
         if row_n < 10:
             fig, ax = plt.subplots()
@@ -62,7 +102,7 @@ for row_n, cpt in cpt_df.iterrows():
             ax.set_ylabel("Depth")
             ax.set_xlabel("fs")
             ax.grid()
-            fig.savefig(plot_dir / f"{cpt.CombinedName}_fs.png")
+            fig.savefig(plot_dir / f"{cpt_name}_fs.png")
             plt.close(fig)
 
             fig, ax = plt.subplots()
@@ -76,20 +116,28 @@ for row_n, cpt in cpt_df.iterrows():
             ax.set_ylabel("Depth")
             ax.set_xlabel("Vs (m/s)")
             ax.set_xlim(0, 600)
-            fig.savefig(plot_dir / f"{cpt.CombinedName}_Vs.png")
+            fig.savefig(plot_dir / f"{cpt_name}_Vs.png")
             plt.close(fig)
-        elif row_n < 10000:
+        elif row_n > 0:
             pass
         else:
             break
     else:
-        skipped_fp.write(f"{cpt.CombinedName} - CPT file not found\n")
+        log_error(skipped_fp, cpt_name, f"CPT file not found")
 
+mpi_results = comm.gather(results, root=master)
 
-result_df = pd.DataFrame.from_dict(results, orient="index")
-result_df.to_csv(out_dir / "vs30_results.csv")
+if is_master:
+    master_results = {}
+    for result_dict in mpi_results:
+        master_results.update(result_dict)
+    result_df = pd.DataFrame.from_dict(master_results, orient="index")
+    result_df.to_csv(out_dir / "vs30_results.csv")
 
-result_df.plot.scatter("Vs30", "Vsz")
-fig = plt.gcf()
-fig.savefig(plot_dir / "vs30_vsz_scatter.png")
-input()
+    ll = geo.wgs_nztm2000x(result_df[["NZTM_X", "NZTM_Y"]])
+    result_df["lon"] = ll[:, 0]
+    result_df["lat"] = ll[:, 1]
+
+    result_df.plot.scatter("Vs30", "Vsz")
+    fig = plt.gcf()
+    fig.savefig(plot_dir / "vs30_vsz_scatter.png")
