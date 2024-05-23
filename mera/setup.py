a96253f3c111ca5617a8d5974c29202f385a8fab
Claudio <claudio.schill@canterbury.ac.nz>
2024-05-23 16:12:59 +1200

    Remove mera and pygmt_helper

--- mera/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="mera",
-    version="22.8.1",
-    packages=find_packages(),
-    url="",
-    description="Code for GM prediction mixed-effects regression analysis",
-)
